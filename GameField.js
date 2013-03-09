5faea6e52a1a011be09522e20689069afa175ed8
OpenShift guest <mikhail.ruchkin@gmail.com>
2013-03-09 15:06:17 -0500

    added logic of types uniting

--- GameField.js
+++ GameField.js
@@ -40,6 +40,14 @@ function GameField(canvas) {
         };
         return false;
     };
+    /*canvas.onclick = resizeCanvas;
+    
+    function resizeCanvas() {
+        var oldImg = fieldContext.getImageData(0, 0, canvas.width, canvas.height);
+        fieldContext.clearRect(0,0,canvas.width,canvas.height);
+        fieldContext.scale(2,2);
+        fieldContext.putImageData(oldImg, 0, 0, 0, 0, canvas.width, canvas.height);
+    }*/
     
     /**
     * Показывает доступные места для карточки.
@@ -66,7 +74,7 @@ function GameField(canvas) {
                 }
             }
         }
-    }
+    };
     
     /**
     * Очищает доступные места.
@@ -83,7 +91,7 @@ function GameField(canvas) {
                                             canvas.height/2 - 50 + (+ij[1]+directions[dir][1])*100, 100, 100);
             }
         }
-    }
+    };
     
     /**
     * Проверяет, подходит ли карточка на поле в координаты i,j.
@@ -103,7 +111,7 @@ function GameField(canvas) {
                     //console.log('side 1= %d  side 2= %d',(+dir+rot)%4,((+dir+newCard[dir].rotate)%4 + 2)%4);
                    // console.log('dir= %s rot= %s стороны равны= %s',dir,rot,(card.sides.names[(+dir+rot)%4] === newCard[dir].sides.names[((+dir+newCard[dir].rotate)%4 + 2)%4]));
                 }
-                if(!newCard[dir] || (card.sides.names[(+dir+rot)%4] === newCard[dir].sides.names[/*((+dir+newCard[dir].rotate)%4 + 2)%4*/(4+(+dir+2)%4 - newCard[dir].rotate)%4])) checked++;
+                if(!newCard[dir] || (card.sides.names[(+dir+rot)%4] === newCard[dir].sides.names[(4+(+dir+2)%4 - newCard[dir].rotate)%4])) checked++;
                 else break;
                // console.log('checked=' +checked);
                 if(checked == 4) return true;
@@ -112,21 +120,31 @@ function GameField(canvas) {
         return false;
     };
     
+    function showTable(){
+        cityMonitor.innerHTML = types.C.length;
+        fieldMonitor.innerHTML = types.F.length;
+        roadMonitor.innerHTML = types.R.length;
+        monkMonitor.innerHTML = types.M.length;
+    }
     /**
     * Ставит карточки на поле координаты i,j с поворотом -rotate*PI/2 радиан.
     */ 
     this.setCard = function (card,i,j,rotate) {
+        console.log("cart %d %d begin",i,j);
         fieldContext.save();
         fieldContext.translate(canvas.width/2+i*card.height, canvas.height/2+j*card.height);
         fieldContext.rotate(rotate*Math.PI/2);   
         fieldContext.drawImage(card.image,-card.height / 2,-card.height / 2,card.height,card.width); 
         fieldContext.restore();
-        drawPoints(card,i,j,rotate);
         card.rotate = rotate;
         cardTable[i+'.'+j] = card;
+        unionTypes(card,i,j);
         pushTypes(card);
-        console.dir(cardTable);
-        console.dir(types);
+        drawPoints(card,i,j,rotate);
+        console.log("cart %d %d placed",i,j);
+        //console.dir(cardTable);
+        //console.dir(types);
+        showTable();
     };
     
     /**
@@ -152,16 +170,51 @@ function GameField(canvas) {
     * Объединяет типы.
     */ 
     var unionTypes = function (card,i,j) {
-        
+        var actualType,
+            thisObj,
+            otherObj;
+        for(var k = 0; k < 4; k++) {
+            //console.log("k= %d",k);
+            //console.log("newcard= %s",i+directions[k][0]+'.'+(j+directions[k][1]));
+            var newCard = cardTable[i+directions[k][0]+'.'+(j+directions[k][1])];          
+            if(newCard === undefined) continue;
+            var otherSide = (k+2+card.rotate)%4;
+            var thisSide = (4 - newCard.rotate)%4;
+            for(var s = 0, l = card.sides.objs[thisSide].length; s < l; s++) {
+                thisObj = card.objs[card.sides.objs[thisSide][s]-1];
+                otherObj = newCard.objs[newCard.sides.objs[otherSide][l-s-1]-1];
+                //console.log("s= %d",s);
+                //console.log("thisside= %s , otherside= %s",thisSide,otherSide);
+                //console.log("l1= %d  l2= %d  l-s-1= %d",card.sides.objs[thisSide].length,newCard.sides.objs[otherSide].length,l-s-1);
+                //console.log("thisCard.rotate= %s  newCard.rotate= %s",card.rotate,newCard.rotate);
+                actualType = thisObj.actualType;
+                if(actualType !== undefined)  {
+                    actualType.elems.push(otherObj.actualType.elems);
+                    //delete otherObj.actualType;
+                    otherObj.actualType = actualType;
+                }
+                else  {                    
+                    otherObj.actualType.elems.push(thisObj);                   
+                    thisObj.actualType = otherObj.actualType;
+                    //console.log("создается тип= %s",i);
+                }
+            }
+        }
     };
     
     /**
     * Заносит элементы карточки в массив типов.
     */ 
     var pushTypes = function (card) {
+        //if(!card.actualTypes.length) card.actualTypes = [];
         for(var i in card.objs) {
-            var newType = Type.factory(card.objs[i].type);
-            types[card.objs[i].type.slice(0,1)].push(newType);
+            if(card.objs[i].actualType === undefined) {
+                console.log("создается тип= %s",i);
+                var newType = Type.factory(card.objs[i].type);
+                newType.elems.push(card.objs[i]);
+                types[card.objs[i].type.slice(0,1)].push(newType);
+                card.objs[i].actualType = newType;
+            }     
         }
     };  
 }
