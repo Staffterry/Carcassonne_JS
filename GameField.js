e0c313cd12ae26cfda991cbc315a7adc6c20d137
OpenShift guest <mikhail.ruchkin@gmail.com>
2013-04-13 17:49:32 -0400

    test event handling

--- GameField.js
+++ GameField.js
@@ -16,12 +16,30 @@ function GameField(canvas) {
         fieldContext = canvas.getContext('2d'),
         currentCard,
         copiedCanvas,
-        currentScale,
+        currentScale = 1,
         self = this;       
     
     /**
     * драг ын дроп игрового поля.
     */ 
+    canvas.onclick = function(e) {
+        var rect = canvas.getBoundingClientRect();
+        var canvasX = e.clientX - rect.left;
+        var canvasY = e.clientY - rect.top;
+       // alert(fieldContext.isPointInPath(canvasX,canvasY));
+       // if(fieldContext.isPointInPath(canvasX,canvasY)) {
+            var detectedColorData = fieldContext.getImageData(canvasX,canvasY,1,1);
+            var detectedColor = 'rgba('+detectedColorData.data[0]+','+detectedColorData.data[1]+','+detectedColorData.data[2]+','+detectedColorData.data[3]+')';
+            if(detectedColor === 'rgba(0,0,0,255)') alert('uspeh');
+            else if(detectedColor === 'rgba(255,204,204,255)') alert('uspeh2');
+            else return;
+      //  }
+        //else alert("nope");
+        /*if(fieldContext.isPointInPath(2510,3070))
+            alert("okay");
+        else alert("nope");*/
+    }
+    
     canvas.onmousedown = function(e) {
         var mouseCoords = {
             x: e.clientX,
@@ -29,13 +47,9 @@ function GameField(canvas) {
         };
         var oldLeft = parseInt(getComputedStyle(canvas).left) || 0;
         var oldTop = parseInt(getComputedStyle(canvas).top) || 0;
-        //alert(mouseCoords.x +"  "+ mouseCoords.y);
         canvas.onmousemove = function(event) {
-            //console.log("%d vs %d",canvas.style.left,canvas.style.top);
             canvas.style.left = oldLeft+event.clientX-mouseCoords.x+'px';
             canvas.style.top = oldTop+event.clientY-mouseCoords.y+'px';
-            //canvas.(event.clientX-mouseCoords.x,event.clientY-mouseCoords.y);
-            //console.log("%d  %d",event.clientX-mouseCoords.x,event.clientY-mouseCoords.y);
             return false;
         };
         document.onmouseup = function() {
@@ -45,39 +59,56 @@ function GameField(canvas) {
         return false;
     };
     
-    canvas.onmousewheel = canvas.onwheel = resizeCanvas;
+    canvas.onmousewheel = canvas.onwheel = canvas.onDOMMouseScroll = resizeCanvas;
+    var container = document.getElementById('field-wrapper');
+    container.onclick = function(e) {
+        if(e.target.id == 'plus') resizeCanvas(e,1.3);
+        else if(e.target.id == 'minus') resizeCanvas(e,-1.3);
+        else return;
+        return false;
+    };
     
-    function resizeCanvas(e) {
+    function resizeCanvas(e,scale) {         //попробовать вызывать redraw() всех компонент
         console.log('robit');
-        currentScale = currentScale || 1;
-        var delta = e.wheelDelta ? e.wheelDelta/100 : e.detail ? -e.detail : 0;
+        //currentScale = currentScale || 1;
+        var delta = scale ? scale : e.wheelDelta ? e.wheelDelta/100 : e.detail ? -e.detail :  0;
         console.log(delta);
-        if(!copiedCanvas)  { copiedCanvas = document.createElement('canvas');
-            copiedCanvas.width = canvas.width;
-            copiedCanvas.height = canvas.height;
-            var imageData = fieldContext.getImageData(0, 0, canvas.width, canvas.height);
-            copiedCanvas.getContext("2d").putImageData(imageData, 0, 0);
+        var previousScale = currentScale; 
+        currentScale += (Math.pow(1.1,delta)-1);
+        currentScale = +currentScale.toFixed(1);
+        if(currentScale < 0.4 || currentScale > 1.8) {
+            currentScale = previousScale;
+            return;
         }
         
-        //canvas.style.zoom = canvas.style.zoom || 1;  
-       // canvas.style.zoom = 1.1;
-         
         fieldContext.save();
-        currentScale += +(Math.pow(1.1,delta)-1).toFixed(1);
-        console.log(resizeCanvas.scale);
+        console.log(currentScale);
         var newWidth = canvas.width * currentScale;
         var newHeight = canvas.height * currentScale;
         fieldContext.translate(-((newWidth-canvas.width)/2), -((newHeight-canvas.height)/2));
         fieldContext.scale(currentScale, currentScale);
-        fieldContext.clearRect(0, 0, canvas.width, canvas.height);
-        fieldContext.drawImage(copiedCanvas, 0, 0);
+        /*fieldContext.clearRect(0, 0, canvas.width, canvas.height);
+        fieldContext.drawImage(copiedCanvas, 0, 0);*/
+        self.redraw();
         fieldContext.restore();
         if(currentCard) self.drawRects(currentCard);
     }
     
-    /*function redraw(scale) {
+    this.redraw = function() {
+        //переписовка карточного поля
+        fieldContext.clearRect(0,0,canvas.width,canvas.height);
         
-    }*/
+        for (var card in cardTable) {
+            console.log("card %s redrawing ", card);
+            redrawCard(cardTable[card],card.split('.')[0],card.split('.')[1],cardTable[card].rotate);
+            drawPoints(cardTable[card],card.split('.')[0],card.split('.')[1],cardTable[card].rotate);
+        }
+        //if(currentCard) self.drawRects(currentCard);
+        
+        //перерисовка точек(*)
+        //перерисовка квадратов
+        //перерисовка миплов(*)
+    }
     
     /**
     * Показывает доступные места для карточки.
@@ -96,8 +127,8 @@ function GameField(canvas) {
                 if(this.checkCard(card,+ij[0]+directions[dir][0],+ij[1]+directions[dir][1])){
                    // console.log('empty and checked');
                     fieldContext.beginPath();
-                    fieldContext.rect(canvas.width/2 - curCardSize/2 + (+ij[0]+directions[dir][0])*curCardSize+1, 
-                                            canvas.height/2 - curCardSize/2 + (+ij[1]+directions[dir][1])*curCardSize+1, 98*currentScale,98*currentScale);
+                    fieldContext.rect(canvas.width/2 - curCardSize/2 + (+ij[0]+directions[dir][0])*curCardSize+1*currentScale, 
+                                            canvas.height/2 - curCardSize/2 + (+ij[1]+directions[dir][1])*curCardSize+1*currentScale, 98*currentScale,98*currentScale);
                     fieldContext.fillStyle = '#ffcccc';
                     fieldContext.fill();
                     fieldContext.lineWidth = 1;
@@ -135,15 +166,7 @@ function GameField(canvas) {
         for(var rot = 0; rot < 4; rot++) {
             checked = 0;
             for(var dir in directions) {
-                newCard[dir] = /*rot > 0 ? newCard[dir] :*/ cardTable[(i+directions[dir][0])+'.'+(j+directions[dir][1])];
-               // if(!newCard[dir]) continue;
-                //if(!newCard[dir]) console.log('cardTable['+(i+directions[dir][0])+'.'+(j+directions[dir][1])+'] empty square ok');
-                /*else {
-                   // console.log('cardTable['+(i+directions[dir][0])+'.'+(j+directions[dir][1])+']');
-                    //console.log('%s  %s dir= %d dir2= %d',card.sides.names[(+dir+rot)%4],newCard[dir].sides.names[(+dir+2)%4],dir,((+dir+newCard[dir].rotate)%4 + 2)%4);
-                    //console.log('side 1= %d  side 2= %d',(+dir+rot)%4,((+dir+newCard[dir].rotate)%4 + 2)%4);
-                   // console.log('dir= %s rot= %s стороны равны= %s',dir,rot,(card.sides.names[(+dir+rot)%4] === newCard[dir].sides.names[((+dir+newCard[dir].rotate)%4 + 2)%4]));
-                }*/
+                newCard[dir] =  cardTable[(i+directions[dir][0])+'.'+(j+directions[dir][1])];
                 if(!newCard[dir] || (card.sides.names[(+dir+rot)%4] === newCard[dir].sides.names[(4+(+dir+2)%4 - newCard[dir].rotate)%4])) checked++;
                 else break;
                // console.log('checked=' +checked);
@@ -153,6 +176,7 @@ function GameField(canvas) {
         return false;
     };
     
+    //тестовая функция для вывода кол-ва всех типов на карте
     function showTable(){
         cityMonitor.innerHTML = types.C.length;
         fieldMonitor.innerHTML = types.F.length;
@@ -162,13 +186,18 @@ function GameField(canvas) {
     /**
     * Ставит карточки на поле координаты i,j с поворотом -rotate*PI/2 радиан.
     */ 
-    this.setCard = function (card,i,j,rotate) {
-        console.log("cart %d %d begin",i,j);
+    function redrawCard(card,i,j,rotate) {
         fieldContext.save();
         fieldContext.translate(canvas.width/2+i*card.height, canvas.height/2+j*card.height);
         fieldContext.rotate(rotate*Math.PI/2);   
         fieldContext.drawImage(card.image,-card.height / 2,-card.height / 2,card.height,card.width); 
-        fieldContext.restore();
+        fieldContext.restore();      
+    }
+    
+    
+    this.setCard = function (card,i,j,rotate) {
+        console.log("cart %d %d begin",i,j);
+        redrawCard(card,i,j,rotate);
         card.rotate = rotate;
         cardTable[i+'.'+j] = card;
         unionTypes(card,i,j);
@@ -183,12 +212,12 @@ function GameField(canvas) {
     /**
     * Отрисовывает точки на карточке.
     */ 
-    var drawPoints = function (card,i,j,rotate) {
+    var drawPoints = function (card,i,j,rotate) {        
         for(var obj in card.objs) {
             var half = card.height/2;
             var newX = canvas.width/2+half*2*i+(card.objs[obj].point.x-half)*Math.cos(-rotate*Math.PI/2)+(card.objs[obj].point.y-half)*Math.sin(-rotate*Math.PI/2);
             var newY = canvas.height/2+half*2*j+(card.objs[obj].point.y-half)*Math.cos(-rotate*Math.PI/2)-(card.objs[obj].point.x-half)*Math.sin(-rotate*Math.PI/2);
-            //console.log("%d  %d",newX,newY);
+            console.log("%d  %d",newX,newY);
             fieldContext.beginPath();
             fieldContext.arc(newX,newY, 4, 0, 2 * Math.PI, false);
             fieldContext.fillStyle = '#000000';
